b56956b37be3524f2cced464516cd24c8d673dbe
Nikita Bobko <nikitabobko@gmail.com>
2019-05-27 11:49:13 +0300

    Move F back to 0.5

--- es_distributed/es.py
+++ es_distributed/es.py
@@ -259,8 +259,8 @@ def run_master(log_dir, exp, num_workers, sockets):
         population, efficiency = differential_evolution_one_step(differential_evolution_one_step_objective_function,
                                                                  population,
                                                                  population_values=efficiency,
-                                                                 crossover_prob=crossover_prob,
-                                                                 differential_weight=random.uniform(0.5, 1.0))
+                                                                 crossover_prob=crossover_prob)
+        # todo check such variant differential_weight=random.uniform(0.5, 1.0)
 
         # noinspection PyTypeChecker
         theta = population[np.argmax(efficiency)]
