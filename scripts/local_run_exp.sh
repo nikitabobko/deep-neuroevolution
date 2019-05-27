310b302705ea3bcb78f4f41a9fc84b2b086b8937
Nikita Bobko <nikitabobko@gmail.com>
2019-05-27 20:34:29 +0300

    200 workers

--- scripts/local_run_exp.sh
+++ scripts/local_run_exp.sh
@@ -2,6 +2,6 @@
 NAME=exp_`date "+%m_%d_%H_%M_%S"`
 ALGO=$1
 EXP_FILE=$2
-NUM_WORKERS=40
+NUM_WORKERS=200
 
 python -m es_distributed.main master --algo "$ALGO" --num_workers "$NUM_WORKERS" --exp_file "$EXP_FILE"
